ced096632640ad62186cabdd9e7d6d4c53a4cb98
tariq <tariqshariff.ts@gmail.com>
2022-03-21 19:35:37 +0530

    build board

--- chess-board/index.js
+++ chess-board/index.js
@@ -1,1 +1,37 @@
-console.log("Script.js");
+const BOARD_SIZE = 8;
+
+const boardElement = document.getElementById("board");
+
+function createCell(color) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.style.backgroundColor = color;
+  return cell;
+}
+
+function generateGrid(size) {
+  const grid = [];
+  for (let r = 0; r < size; r++) {
+    grid[r] = [];
+    for (let c = 0; c < size; c++) {
+      let cellColor = (r + c) % 2 ? "black" : "white";
+      grid[r][c] = createCell(cellColor);
+    }
+  }
+  return grid;
+}
+
+function renderBoard(grid) {
+  let rowElement;
+  for (let r = 0; r < grid.length; r++) {
+    rowElement = document.createElement("div");
+    rowElement.classList.add("row");
+    boardElement.append(rowElement);
+    for (let c = 0; c < grid.length; c++) {
+      rowElement.append(grid[r][c]);
+    }
+  }
+}
+
+const grid = generateGrid(BOARD_SIZE);
+renderBoard(grid);
