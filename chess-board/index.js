ad17c361c33903465722f12b1cdb8422e3f865c7
tariq <tariqshariff.ts@gmail.com>
2022-03-21 21:16:43 +0530

    complete chess-board challenge

--- chess-board/index.js
+++ chess-board/index.js
@@ -2,20 +2,32 @@ const BOARD_SIZE = 8;
 
 const boardElement = document.getElementById("board");
 
-function createCell(color) {
+function createCell(rowId, colId, color) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.dataset.row = rowId;
+  cell.dataset.col = colId;
   cell.style.backgroundColor = color;
   return cell;
 }
 
 function generateGrid(size) {
-  const grid = [];
+  let grid = [];
   for (let r = 0; r < size; r++) {
     grid[r] = [];
     for (let c = 0; c < size; c++) {
       let cellColor = (r + c) % 2 ? "black" : "white";
-      grid[r][c] = createCell(cellColor);
+      grid[r][c] = createCell(r, c, cellColor);
+    }
+  }
+  return grid;
+}
+
+function resetGrid(grid) {
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid.length; c++) {
+      let cellColor = (r + c) % 2 ? "black" : "white";
+      grid[r][c].style.backgroundColor = cellColor;
     }
   }
   return grid;
@@ -32,6 +44,41 @@ function renderBoard(grid) {
     }
   }
 }
+
+function selectDiagonals(grid, { r, c }) {
+  const selectionColor = "red";
+  grid[r][c].style.backgroundColor = selectionColor;
+
+  let row, col;
+  for (row = r - 1, col = c + 1; row >= 0 && col < grid.length; row--, col++) {
+    grid[row][col].style.backgroundColor = selectionColor;
+  }
+  for (
+    row = r + 1, col = c + 1;
+    row < grid.length && col < grid.length;
+    row++, col++
+  ) {
+    grid[row][col].style.backgroundColor = selectionColor;
+  }
+  for (row = r + 1, col = c - 1; row < grid.length && col >= 0; row++, col--) {
+    grid[row][col].style.backgroundColor = selectionColor;
+  }
+  for (row = r - 1, col = c - 1; row >= 0 && col >= 0; row--, col--) {
+    grid[row][col].style.backgroundColor = selectionColor;
+  }
+}
+
+function handleCellClick(event) {
+  const cell = event.target;
+  const cellPosition = {
+    r: +cell.dataset.row,
+    c: +cell.dataset.col,
+  };
+  resetGrid(grid);
+  selectDiagonals(grid, cellPosition);
+}
+
+boardElement.addEventListener("click", handleCellClick);
 
 const grid = generateGrid(BOARD_SIZE);
 renderBoard(grid);
